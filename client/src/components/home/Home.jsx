35d241e88d6c7531e64202c5f9f2a99f8afc7f56
Shruti3004 <mail2shruti.ag@gmail.com>
2020-09-25 18:20:00 +0530

    added carousel part

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeCarousel from "./HomeCarousel";
 import HomeCards from "./HomeCards";
+import NewHomePage from "./NewHomePage";
 
 function Home() {
   return (
     <React.Fragment>
       <HomeCarousel />
       <HomeCards />
+      <NewHomePage />
     </React.Fragment>
   );
 }
