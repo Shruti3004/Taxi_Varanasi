17908d3b713ebaf8a27ebcbcba2f499d978bd7cb
Shruti3004 <mail2shruti.ag@gmail.com>
2020-09-26 03:29:24 +0530

    updated slack-label

--- client/src/components/home/NewHomePage.jsx
+++ client/src/components/home/NewHomePage.jsx
@@ -3,6 +3,7 @@ import "./newHomePage.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import auto from "../../images/auto.png";
 
 function NewHomePage() {
   const [nav1, setNav1] = useState(null);
@@ -22,17 +23,29 @@ function NewHomePage() {
     fade: true,
     dots: true,
     asNavFor: ".slider-nav",
+    autoplay: true,
+    autoplaySpeed: 4000,
+    mobileFirst: true,
   };
 
   const settingsThumbs = {
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: ".slider-for",
     // dots: true,
     centerMode: true,
     swipeToSlide: true,
     focusOnSelect: true,
-    // centerPadding: "-20px",
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   const slidesData = [
@@ -69,40 +82,61 @@ function NewHomePage() {
   ];
 
   return (
-    <div className="App">
-      <div className="slider-wrapper">
-        <div className="thumbnail-slider-wrap">
-          <Slider
-            {...settingsThumbs}
-            asNavFor={nav1}
-            ref={(slider) => setSlider2(slider)}
-          >
-            {slidesData.map((slide) => (
-              <div className="slick-slide" key={slide.id}>
-                <img
-                  className="slick-slide-image"
-                  src={`https://picsum.photos/800/400?img=${slide.id}`}
-                />
-              </div>
-            ))}
-          </Slider>
-        </div>
-        <Slider
-          {...settingsMain}
-          asNavFor={nav2}
-          ref={(slider) => setSlider1(slider)}
-        >
-          {slidesData.map((slide) => (
-            <div className="slick-slide" key={slide.id}>
-              <h2 className="slick-slide-title">{slide.title}</h2>
-              <img
-                className="slick-slide-image img-fluid"
-                src={`https://picsum.photos/800/400?img=${slide.id}`}
-              />
-              <label className="slick-slide-label">{slide.label}</label>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 col-md-12 col-sm-12 col-12 mb-5">
+          <div className="slider-wrapper">
+            <div className="thumbnail-slider-wrap">
+              <Slider
+                {...settingsThumbs}
+                asNavFor={nav1}
+                ref={(slider) => setSlider2(slider)}
+              >
+                {slidesData.map((slide) => (
+                  <div className="slick-slide" key={slide.id}>
+                    <img
+                      className="slick-slide-image-thumbnail py-3"
+                      src={auto}
+                    />
+                  </div>
+                ))}
+              </Slider>
             </div>
-          ))}
-        </Slider>
+            <Slider
+              {...settingsMain}
+              asNavFor={nav2}
+              ref={(slider) => setSlider1(slider)}
+            >
+              {slidesData.map((slide) => (
+                <div className="slick-slide" key={slide.id}>
+                  <h2 className="slick-slide-title">{slide.title}</h2>
+                  <img
+                    className="slick-slide-image img-fluid"
+                    src={`https://picsum.photos/800/400?img=${slide.id}`}
+                  />
+                  <label className="slick-slide-label">
+                    <table className="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th className="active text-white">City Ride</th>
+                          <th className="active text-white">Outstation</th>
+                          <th className="active text-white">Airport</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>City Ride</td>
+                          <td>Outstation</td>
+                          <td>Airport</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </label>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </div>
       </div>
     </div>
   );
